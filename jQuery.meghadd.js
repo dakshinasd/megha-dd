9107e9ea9e07708fa75270ab0fad4ddde0864d4f
dakshinasd <me@dakshinasd.com>
2016-05-17 07:01:04 +0530

    Small modifications to JS

--- jQuery.meghadd.js
+++ jQuery.meghadd.js
@@ -4,14 +4,23 @@ jQuery.fn.meghadd = function(){
       var option = [],
           counter = 0;
 
+      //adding all the element into one array
       $(this).find('option').each(function(){
         option[counter] = {
             name : $(this).text(),
-            value : $(this).val()
+            value : $(this).val(),
+
+            //---- TI 1.0 Capture Attributes ----
+            // attr : $(this).attr()
         };
 
         counter++
       });
+
+      //hiding the original dd
+      el.css('display','none');
+
+      el.parent().append('<div class="megha-dd-wrapper"><a href="#">Hello</a></div>');
 
       console.log(option);
   })
